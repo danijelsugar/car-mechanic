fbcffb35aa23bf728044350816558e4ea502ff22
Danijel Sugar <danijel.sugar@outlook.com>
2018-10-09 16:06:01 +0200

    Sticky menu, mobile menu, hover div

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
+
+// FUNCTION GO SMOOTH TO TOP OF PAGE
 $('#arrow-up').on('click', function() {
   const top = $('#header').position().top;
 
@@ -9,6 +11,7 @@ $('#arrow-up').on('click', function() {
   );
 });
 
+// FUNCTION GO SMOOTH TO SERVICES
 $('#to-services').on('click', function() {
   const services = $('#usluge').position().top;
 
@@ -19,3 +22,100 @@ $('#to-services').on('click', function() {
     900
   );
 });
+
+// STICKY NAVBAR
+window.onscroll = function() {myFunction()};
+
+    var header = document.getElementById("myNav");
+    var sticky = header.offsetTop;
+
+    function myFunction() {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
+
+// MOBILE MENU
+
+$( '#dl-menu' ).dlmenu({
+  animationClasses : { classin : 'animation-class-name', classout : 'animation-class-name' }
+});
+  
+
+// CHANGE THE DIV CONTENT
+
+$('.caption').hide()
+
+$("#service-1").hover(
+  function () {
+    $('#caption-1').stop().fadeTo("slow", 1);
+  },
+  function () {
+    $('#caption-1').stop().fadeOut("slow");
+  }
+);
+
+$("#service-2").hover(
+  function () {
+    $('#caption-2').stop().fadeTo("slow", 1);
+  },
+  function () {
+    $('#caption-2').stop().fadeOut("slow");
+  }
+);
+
+$("#service-3").hover(
+  function () {
+    $('#caption-3').stop().fadeTo("slow", 1);
+  },
+  function () {
+    $('#caption-3').stop().fadeOut("slow");
+  }
+);
+
+$("#service-4").hover(
+  function () {
+    $('#caption-4').stop().fadeTo("slow", 1);
+  },
+  function () {
+    $('#caption-4').stop().fadeOut("slow");
+  }
+);
+
+$("#service-5").hover(
+  function () {
+    $('#caption-5').stop().fadeTo("slow", 1);
+  },
+  function () {
+    $('#caption-5').stop().fadeOut("slow");
+  }
+);
+
+$("#service-6").hover(
+  function () {
+    $('#caption-6').stop().fadeTo("slow", 1);
+  },
+  function () {
+    $('#caption-6').stop().fadeOut("slow");
+  }
+);
+
+$("#service-7").hover(
+  function () {
+    $('#caption-7').stop().fadeTo("slow", 1);
+  },
+  function () {
+    $('#caption-7').stop().fadeOut("slow");
+  }
+);
+
+$("#service-8").hover(
+  function () {
+    $('#caption-8').stop().fadeTo("slow", 1);
+  },
+  function () {
+    $('#caption-8').stop().fadeOut("slow");
+  }
+);
